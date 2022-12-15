defc3bfad4a07cd743f8f51d5e7f744ea108d445
we45-LT45 <we45-lt45@Iclusters-MacBook-Pro.local>
2022-12-15 18:12:49 +0530

    added headless feature for chrome in conftest

--- conftest.py
+++ conftest.py
@@ -9,12 +9,15 @@ from pytest import mark
 from dotenv import load_dotenv
 import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(autouse=True, scope='class')
 def setup(request):
     load_dotenv()
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    option = Options()
+    option.add_argument('--headless')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
     driver.get(os.getenv('BASE_URL'))
     driver.maximize_window()
     request.cls.driver = driver
